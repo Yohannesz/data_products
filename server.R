8e754a3711c0a1b15877d70886e8aa5f49c72f7b
Yohannesz <yohannes.live@gmail.com>
2014-09-15 21:16:43 -0600

    Update server.R

--- server.R
+++ server.R
@@ -5,7 +5,9 @@ cdata<- read.xlsx("crime.xlsx", 1 , stringsAsFactors=F, header=TRUE)
 IndexData <- cdata
 IndexData$Year <- factor(IndexData$year)
 IndexData$Crime_index<-as.numeric(IndexData$crime_index)
-# Define server logic required to plot various variables against Crime index
+SortIndex<-cdata[order(-cdata$crime_index), ]
+SortIndex$year<-factor(SortIndex$year)
+# Define server logic required to plot the variables against Crime index
 shinyServer(function(input, output) {
         
    
@@ -25,5 +27,8 @@ shinyServer(function(input, output) {
                         data = IndexData, las=2, par(mar = c(14, 5, 4, 2)+ 0.1), col="red",
                         #horzontal=TRUE, varwidth=TRUE,
                         cex.lab=1.3, outline = input$outliers)
+        })
+        output$view <- renderTable({
+                head(SortIndex, n = 5)
         })
 })
